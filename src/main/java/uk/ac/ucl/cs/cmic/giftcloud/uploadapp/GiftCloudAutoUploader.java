9cd1e4a33ccdd457604cf0ecdc6af9f04b707e1a
Tom Doel <t.doel@ucl.ac.uk>
2015-04-15 15:18:09 +0100

    Shared up duplicate code

--- src/main/java/uk/ac/ucl/cs/cmic/giftcloud/uploadapp/GiftCloudAutoUploader.java
+++ src/main/java/uk/ac/ucl/cs/cmic/giftcloud/uploadapp/GiftCloudAutoUploader.java
@@ -8,7 +8,9 @@ import uk.ac.ucl.cs.cmic.giftcloud.data.Project;
 import uk.ac.ucl.cs.cmic.giftcloud.data.Session;
 import uk.ac.ucl.cs.cmic.giftcloud.data.SessionVariable;
 import uk.ac.ucl.cs.cmic.giftcloud.dicom.MasterTrawler;
-import uk.ac.ucl.cs.cmic.giftcloud.restserver.*;
+import uk.ac.ucl.cs.cmic.giftcloud.restserver.RestServerHelper;
+import uk.ac.ucl.cs.cmic.giftcloud.restserver.SeriesImportFilterApplicatorRetriever;
+import uk.ac.ucl.cs.cmic.giftcloud.restserver.SubjectAliasMap;
 import uk.ac.ucl.cs.cmic.giftcloud.uploadapplet.SwingProgressMonitor;
 import uk.ac.ucl.cs.cmic.giftcloud.uploadapplet.SwingUploadFailureHandler;
 import uk.ac.ucl.cs.cmic.giftcloud.util.OneWayHash;
@@ -16,7 +18,6 @@ import uk.ac.ucl.cs.cmic.giftcloud.util.OneWayHash;
 import java.awt.*;
 import java.io.File;
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.*;
 import java.util.List;
@@ -24,9 +25,9 @@ import java.util.concurrent.*;
 
 public class GiftCloudAutoUploader {
 
-    private GiftCloudReporter reporter;
-    private RestServerHelper restServerHelper;
-    private Container container;
+    private final GiftCloudReporter reporter;
+    private final RestServerHelper restServerHelper;
+    private final Container container;
 
     private final String giftCloudServerUrl;
 
@@ -53,27 +54,31 @@ public class GiftCloudAutoUploader {
     private final SubjectAliasMap subjectAliasMap;
 
 
-    public GiftCloudAutoUploader(final Container container, final GiftCloudProperties giftCloudProperties, final GiftCloudReporter reporter) throws MalformedURLException {
+    /**
+     * This class is used to automatically and asynchronously group and upload multiple files to a GIFT-Cloud server
+     *
+     * @param restServerHelper
+     * @param giftCloudServerUrl the URL of the GIFT-Cloud server. Must be a valid URL and not null or blank
+     * @param container
+     * @param reporter
+     */
+    public GiftCloudAutoUploader(final RestServerHelper restServerHelper, final String giftCloudServerUrl, final Container container, final GiftCloudReporter reporter) {
+        this.restServerHelper = restServerHelper;
         this.reporter = reporter;
         this.container = container;
-        giftCloudServerUrl = giftCloudProperties.getGiftCloudUrl().get();
-
-        if (StringUtils.isBlank(giftCloudServerUrl)) {
-            throw new MalformedURLException("Please set the URL for the GIFT-Cloud server.");
-        }
-
-        final RestServer restServer = new RestServer(giftCloudProperties, giftCloudServerUrl, reporter);
-        restServerHelper = new RestServerHelper(restServer, reporter);
+        this.giftCloudServerUrl = giftCloudServerUrl;
         subjectAliasMap = new SubjectAliasMap(restServerHelper);
     }
 
-    public String getUrl() {
-        return giftCloudServerUrl;
+    public boolean uploadToGiftCloud(Vector<String> paths, final String projectName) throws IOException {
+        return uploadOrAppend(paths, projectName, false);
     }
 
-    public boolean uploadToGiftCloud(Vector<String> paths, final String projectName) throws IOException {
+    public boolean appendToGiftCloud(Vector<String> paths, final String projectName) throws IOException {
+        return uploadOrAppend(paths, projectName, true);
+    }
 
-
+    private boolean uploadOrAppend(Vector<String> paths, final String projectName, final boolean append) throws IOException {
         SeriesImportFilterApplicatorRetriever filter;
         try {
             if (StringUtils.isEmpty(projectName)) {
@@ -86,6 +91,7 @@ public class GiftCloudAutoUploader {
             throw new IOException("Error encountered retrieving series import filters", exception);
         }
 
+
         final SwingProgressMonitor progress = new SwingProgressMonitor(container, "Finding data files", "searching", 0, paths.size());
 
         final EmptyProgress emptyProgress = new EmptyProgress();
@@ -94,7 +100,6 @@ public class GiftCloudAutoUploader {
         for (final String path : paths) {
             fileList.add(new File(path));
         }
-
 
         final List<Session> sessions = new MasterTrawler(progress, fileList, filter).call();
 
@@ -110,6 +115,7 @@ public class GiftCloudAutoUploader {
         } catch (IOException e) {
             throw new IOException("Uploading could not be performed. The subject and session map could not be obtained due to the following error: " + e.getMessage(), e);
         }
+
 
         boolean uploadSuccess = true;
 
@@ -147,110 +153,12 @@ public class GiftCloudAutoUploader {
             Future<Boolean> upload = executorService.submit(new Callable<Boolean>() {
                 public Boolean call() {
                     try {
-                        Boolean returnValue = session.uploadTo(projectName, finalSubjectName, restServerHelper, sessionParameters, project, emptyProgress, windowTitle, jsContext, new SwingUploadFailureHandler(), reporter);
-                        return returnValue;
-                    } catch (CancellationException exception) {
-                        // Cancellation is the only type of exception for which we don't attempt to upload any more files
-                        throw exception;
-                    } catch (Exception e) {
-                        // ToDo: We should pass back the exception and catch it lower down
-                        return false;
-                    }
-
-                }
-            });
-
-            try {
-                uploadSuccess = uploadSuccess && upload.get();
-            } catch (InterruptedException e) {
-                final Throwable cause = e.getCause();
-                throw new IOException("Uploading was interrupted due to the following error: " + cause.getMessage(), cause);
-            } catch (ExecutionException e) {
-                final Throwable cause = e.getCause();
-                throw new IOException("Uploading failed due to the following error: " + cause.getMessage(), cause);
-            }
-        }
-
-        return true;
-    }
-
-
-
-    public boolean appendToGiftCloud(Vector<String> paths, final String projectName) throws IOException {
-
-
-        SeriesImportFilterApplicatorRetriever filter;
-        try {
-            if (StringUtils.isEmpty(projectName)) {
-                final Optional<String> emptyProject = Optional.empty();
-                filter = new SeriesImportFilterApplicatorRetriever(restServerHelper, emptyProject);
-            } else {
-                filter = new SeriesImportFilterApplicatorRetriever(restServerHelper, Optional.of(projectName));
-            }
-        } catch (Exception exception) {
-            throw new IOException("Error encountered retrieving series import filters", exception);
-        }
-
-        final SwingProgressMonitor progress = new SwingProgressMonitor(container, "Finding data files", "searching", 0, paths.size());
-
-        final EmptyProgress emptyProgress = new EmptyProgress();
-
-        final Vector<File> fileList = new Vector<File>();
-        for (final String path : paths) {
-            fileList.add(new File(path));
-        }
-
-
-        final List<Session> sessions = new MasterTrawler(progress, fileList, filter).call();
-
-        final ExecutorService executorService = Executors.newCachedThreadPool();
-
-        // Get a list of subjects from the server
-        Map<String, String> subjectMapFromServer;
-        Map<String, String> sessionMapFromServer;
-        try {
-            subjectMapFromServer = restServerHelper.getListOfSubjects(projectName);
-            sessionMapFromServer = restServerHelper.getListOfSessions(projectName);
-
-        } catch (IOException e) {
-            throw new IOException("Uploading could not be performed. The subject and session map could not be obtained due to the following error: " + e.getMessage(), e);
-        }
-
-        boolean uploadSuccess = true;
-
-        for (final Session session : sessions) {
-
-            final String patientId = session.getPatientId();
-            final String studyUid = session.getStudyUid();
-            final String seriesUid = session.getSeriesUid();
-
-            final String subjectName = getSubjectName(projectName, subjectMapFromServer, patientId);
-            final String sessionName = getSessionName(studyUid, sessionMapFromServer);
-            final String scanName = getScanName(seriesUid, sessionMapFromServer);
-
-            final GiftCloudSessionParameters sessionParameters = new GiftCloudSessionParameters();
-            sessionParameters.setAdminEmail("null@null.com");
-            sessionParameters.setSessionLabel(sessionName);
-            sessionParameters.setProtocol("");
-            sessionParameters.setVisit("");
-            sessionParameters.setScan(scanName);
-            sessionParameters.setBaseUrl(new URL(giftCloudServerUrl));
-            sessionParameters.setNumberOfThreads(1);
-            sessionParameters.setUsedFixedSize(true);
-
-            final Project project = new Project(projectName, restServerHelper);
-
-            final LinkedList<SessionVariable> sessionVariables = Lists.newLinkedList(session.getVariables(project, session));
-            sessionParameters.setSessionVariables(sessionVariables);
-
-
-            final Optional<String> windowTitle = Optional.empty();
-            final Optional<JSObject> jsContext = Optional.empty();
-
-            Future<Boolean> upload = executorService.submit(new Callable<Boolean>() {
-                public Boolean call() {
-                    try {
-                        Boolean returnValue = session.appendTo(projectName, subjectName, restServerHelper, sessionParameters, project, emptyProgress, windowTitle, jsContext, new SwingUploadFailureHandler(), reporter);
+                        Boolean returnValue;
+                        if (append) {
+                            returnValue = session.appendTo(projectName, finalSubjectName, restServerHelper, sessionParameters, project, emptyProgress, windowTitle, jsContext, new SwingUploadFailureHandler(), reporter);
+                        } else {
+                            returnValue = session.uploadTo(projectName, finalSubjectName, restServerHelper, sessionParameters, project, emptyProgress, windowTitle, jsContext, new SwingUploadFailureHandler(), reporter);
+                        }
                         return returnValue;
                     } catch (CancellationException exception) {
                         // Cancellation is the only type of exception for which we don't attempt to upload any more files
