dc6b5997789e50cce57d55f5867dcacfc3c5bf69
Tom Doel <t.doel@ucl.ac.uk>
2015-07-06 12:11:10 +0100

    Added new pithy error messages, and a retry flag which is determined by the error

--- src/main/java/uk/ac/ucl/cs/cmic/giftcloud/uploader/GiftCloudException.java
+++ src/main/java/uk/ac/ucl/cs/cmic/giftcloud/uploader/GiftCloudException.java
@@ -6,16 +6,30 @@ import java.util.Optional;
 public class GiftCloudException extends IOException {
     private final GiftCloudUploaderError error;
     private final Optional<String> additionalMessage;
+    private final String pithyMessage;
+    private final boolean allowRetry;
 
     public GiftCloudException(final GiftCloudUploaderError error) {
         super(error.getMessageWithErrorCode());
         this.error = error;
-        additionalMessage = Optional.empty();
+        this.pithyMessage = error.getPithyMessage();
+        this.additionalMessage = Optional.empty();
+        this.allowRetry = error.allowRetry();
     }
 
     public GiftCloudException(final GiftCloudUploaderError error, final String additionalMessage) {
         super(error.getMessageWithErrorCode() + " " + additionalMessage);
         this.error = error;
+        this.pithyMessage = error.getPithyMessage();
         this.additionalMessage = Optional.of(additionalMessage);
+        this.allowRetry = error.allowRetry();
+    }
+
+    public String getPithyMessage() {
+        return pithyMessage;
+    }
+
+    public boolean allowRetry() {
+        return allowRetry;
     }
 }
