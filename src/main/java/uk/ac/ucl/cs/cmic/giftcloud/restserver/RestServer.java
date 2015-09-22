0200cadbadbe5babcc5315e06f2bd0381562aea0
Tom Doel <t.doel@ucl.ac.uk>
2015-09-22 18:59:02 +0100

    Anonymisation is now carried out within the CallableUploader class, rather than the RestServer class. This allows for better integration testing, since the anonymisation can now be tested with a MockRestServer

--- src/main/java/uk/ac/ucl/cs/cmic/giftcloud/restserver/RestServer.java
+++ src/main/java/uk/ac/ucl/cs/cmic/giftcloud/restserver/RestServer.java
@@ -1,9 +1,8 @@
 package uk.ac.ucl.cs.cmic.giftcloud.restserver;
 
 import org.json.JSONException;
-import org.nrg.dcm.edit.ScriptApplicator;
-import uk.ac.ucl.cs.cmic.giftcloud.dicom.FileCollection;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
@@ -36,12 +35,11 @@ public interface RestServer {
 
     String getPreArcCode(String projectName) throws Exception;
 
-
-    Set<String> uploadZipFile(final String projectLabel, final GiftCloudLabel.SubjectLabel subjectLabel, SessionParameters sessionParameters, boolean useFixedSizeStreaming, FileCollection fileCollection, Iterable<ScriptApplicator> applicators) throws Exception;
+    Set<String> uploadZipFile(final String projectLabel, final GiftCloudLabel.SubjectLabel subjectLabel, SessionParameters sessionParameters, final File temporaryFile) throws Exception;
 
     void createSubjectAliasIfNotExisting(final String projectLabel, final GiftCloudLabel.SubjectLabel subjectLabel, final String hashedPatientId) throws IOException;
 
-    void appendZipFileToExistingScan(String projectLabel, GiftCloudLabel.SubjectLabel subjectLabel, SessionParameters sessionParameters, XnatModalityParams xnatModalityParams, boolean useFixedSizeStreaming, FileCollection fileCollection, Iterable<ScriptApplicator> applicators) throws Exception;
+    void appendZipFileToExistingScan(String projectLabel, GiftCloudLabel.SubjectLabel subjectLabel, SessionParameters sessionParameters, XnatModalityParams xnatModalityParams, final File temporaryFile) throws Exception;
 
     void resetCancellation();
 
---
+++ src/main/java/uk/ac/ucl/cs/cmic/giftcloud/restserver/UploadFileRequest.java
@@ -0,0 +1,76 @@
+/*=============================================================================
+
+  GIFT-Cloud: A data storage and collaboration platform
+
+  Copyright (c) University College London (UCL). All rights reserved.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.
+
+  See LICENSE.txt in the top level directory for details.
+
+=============================================================================*/
+
+package uk.ac.ucl.cs.cmic.giftcloud.restserver;
+
+import uk.ac.ucl.cs.cmic.giftcloud.util.CloseableResource;
+import uk.ac.ucl.cs.cmic.giftcloud.util.GiftCloudReporter;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Set;
+
+class UploadFileRequest extends HttpRequestWithOutput<Set<String>> {
+    private static final int BUF_SIZE = 4096;
+    private final File temporaryFile;
+
+    UploadFileRequest(final HttpConnectionWrapper.ConnectionType connectionType,
+                      final String url,
+                      final File temporaryFile,
+                      final HttpResponseProcessor responseProcessor,
+                      final GiftCloudProperties giftCloudProperties,
+                      final GiftCloudReporter reporter) {
+        super(connectionType, url, responseProcessor, giftCloudProperties, reporter);
+        this.temporaryFile = temporaryFile;
+    }
+
+
+    @Override
+    protected void prepareConnection(final HttpConnectionBuilder connectionBuilder) throws IOException {
+        super.prepareConnection(connectionBuilder);
+        connectionBuilder.setContentType(HttpConnectionBuilder.CONTENT_TYPE_ZIP);
+
+        final long zipFileSize = temporaryFile.length();
+        connectionBuilder.setFixedLengthStreamingMode(zipFileSize);
+    }
+
+    @Override
+    protected void streamToConnection(final OutputStream outputStream) throws IOException {
+        new CloseableResource<Void, FileInputStream>() {
+            @Override
+            public Void run() throws IOException {
+                resource = new FileInputStream(temporaryFile);
+                writeChunk(resource, outputStream);
+                return null;
+            }
+        }.tryWithResource();
+    }
+
+    @Override
+    protected void cleanup() {
+        if (temporaryFile != null) {
+            temporaryFile.delete();
+        }
+    }
+
+    private void writeChunk(FileInputStream fis, OutputStream os) throws IOException {
+        final byte[] buf = new byte[BUF_SIZE];
+        int chunk;
+        while ((chunk = fis.read(buf)) != -1) {
+            os.write(buf, 0, chunk);
+        }
+    }
+}
