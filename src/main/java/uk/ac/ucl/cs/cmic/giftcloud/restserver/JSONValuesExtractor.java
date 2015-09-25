991024531fea9f9ec58845ad00877a71dbed3671
Tom Doel <t.doel@ucl.ac.uk>
2015-09-25 15:57:26 +0100

    Code tidy

--- src/main/java/uk/ac/ucl/cs/cmic/giftcloud/restserver/JSONValuesExtractor.java
+++ src/main/java/uk/ac/ucl/cs/cmic/giftcloud/restserver/JSONValuesExtractor.java
@@ -2,15 +2,12 @@ package uk.ac.ucl.cs.cmic.giftcloud.restserver;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.Collection;
 import java.util.LinkedHashSet;
 
 final class JSONValuesExtractor implements JSONDecoder<Collection<String>> {
-    private final Logger logger = LoggerFactory.getLogger(JSONValuesExtractor.class);
-    private final Collection<String> c = new LinkedHashSet<String>();
+    private final Collection<String> decodedStrings = new LinkedHashSet<String>();
     private final String key;
 
     JSONValuesExtractor(final String key) {
@@ -21,14 +18,13 @@ final class JSONValuesExtractor implements JSONDecoder<Collection<String>> {
      * (non-Javadoc)
      * @see org.nrg.net.RestOpManager.JSONDecoder#decode(org.json.JSONObject)
      */
-    public void decode(final JSONObject o) throws JSONException {
-        logger.trace("decoding {} from {}", key, o);
-        if (o.has(key)) {
-            c.add((String)(o.get(key)));
+    public void decode(final JSONObject object) throws JSONException {
+        if (object.has(key)) {
+            decodedStrings.add((String) (object.get(key)));
         }
     }
 
     public Collection<String> getResult() {
-        return c;
+        return decodedStrings;
     }
 }
