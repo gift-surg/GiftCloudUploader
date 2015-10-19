d78272ff0ad27ff352361450f7c8ae549faa71e5
Tom Doel <t.doel@ucl.ac.uk>
2015-10-19 17:56:04 +0100

    Added classes describing filters for pixel data anonymisation

---
+++ src/main/java/uk/ac/ucl/cs/cmic/giftcloud/uploader/PixelDataAnonymiseFilter.java
@@ -0,0 +1,26 @@
+package uk.ac.ucl.cs.cmic.giftcloud.uploader;
+
+import java.awt.*;
+import java.awt.geom.Rectangle2D;
+import java.util.List;
+import java.util.Vector;
+
+public class PixelDataAnonymiseFilter {
+    private String filterName;
+    private List<PixelDataAnonymiseFilterRequiredTag> requiredTags;
+    private List<Rectangle2D.Double> redactedShapes;
+
+    public PixelDataAnonymiseFilter(final String filterName, final List<PixelDataAnonymiseFilterRequiredTag> requiredTags, final List<Rectangle2D.Double> redactedShapes) {
+        this.filterName = filterName;
+        this.requiredTags = requiredTags;
+        this.redactedShapes = redactedShapes;
+    }
+
+    public Vector<Shape> getRedactedShapes() {
+        final Vector<Shape> shapes = new Vector<Shape>();
+        for (Rectangle2D.Double redactedRectangle : redactedShapes) {
+            shapes.add(redactedRectangle);
+        }
+        return shapes;
+    }
+}
---
+++ src/main/java/uk/ac/ucl/cs/cmic/giftcloud/uploader/PixelDataAnonymiseFilterRequiredTag.java
@@ -0,0 +1,11 @@
+package uk.ac.ucl.cs.cmic.giftcloud.uploader;
+
+public class PixelDataAnonymiseFilterRequiredTag<T> {
+    private int dicomGroup;
+    private int dicomElement;
+    private T value;
+
+    public PixelDataAnonymiseFilterRequiredTag() {
+
+    }
+}
