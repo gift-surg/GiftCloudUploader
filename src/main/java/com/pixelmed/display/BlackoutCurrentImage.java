1deba728fd3df9605821a7d5fe4d6ac4ac603771
Tom Doel <t.doel@ucl.ac.uk>
2015-10-21 18:54:46 +0100

    Removed unused code

--- src/main/java/com/pixelmed/display/BlackoutCurrentImage.java
+++ src/main/java/com/pixelmed/display/BlackoutCurrentImage.java
@@ -1,20 +1,15 @@
 package com.pixelmed.display;
 
 import com.pixelmed.dicom.*;
-import com.pixelmed.utils.CapabilitiesAvailable;
 import com.pixelmed.utils.FileUtilities;
 
-import java.awt.geom.Rectangle2D;
 import java.io.File;
 import java.io.IOException;
-import java.util.Vector;
 
 public class BlackoutCurrentImage {
 
     private AttributeList list;
     private SourceImage sImg;
-    private File redactedJPEGFile;
-    private BlackoutImage blackoutImage;
 
     public SourceImage getSourceImage() {
         return sImg;
@@ -35,7 +30,6 @@ public class BlackoutCurrentImage {
      * @param    currentFile
      */
     protected void loadDicomFileOrDirectory(File currentFile) throws IOException, DicomException {
-        blackoutImage = new BlackoutImage(currentFile);
         list = readAttributeList(currentFile, true);
         String useSOPClassUID = Attribute.getSingleStringValueOrEmptyString(list, TagFromName.SOPClassUID);
         if (SOPClass.isImageStorage(useSOPClassUID)) {
@@ -45,29 +39,6 @@ public class BlackoutCurrentImage {
         }
     }
 
-
-    public void apply(Vector<Rectangle2D.Double> shapes, boolean burnInOverlays, boolean usePixelPaddingBlackoutValue, boolean useZeroBlackoutValue) throws Exception {
-        if (sImg != null && list != null) {
-            if ((shapes != null && shapes.size() > 0) || burnInOverlays) {
-                String transferSyntaxUID = Attribute.getSingleStringValueOrEmptyString(list, TagFromName.TransferSyntaxUID);
-                if (transferSyntaxUID.equals(TransferSyntax.JPEGBaseline) && !burnInOverlays && CapabilitiesAvailable.haveJPEGBaselineSelectiveBlockRedaction()) {
-                    if (redactedJPEGFile != null) {
-                        redactedJPEGFile.delete();
-                    }
-                    redactedJPEGFile = File.createTempFile("RedactedImage", ".dcm");
-                    ImageEditUtilities.blackoutJPEGBlocks(new File(blackoutImage.getCurrentFileName()), redactedJPEGFile, shapes);
-                    // Need to re-read the file because we need to decompress the redacted JPEG to use to display it again
-                    list = readAttributeList(redactedJPEGFile, true);
-                    // do NOT delete redactedJPEGFile, since will reuse it when "saving", and also file may need to hang around for display of cached pixel data
-                } else {
-                    ImageEditUtilities.blackout(sImg, list, shapes, burnInOverlays, usePixelPaddingBlackoutValue, useZeroBlackoutValue, 0);
-                }
-                sImg = new SourceImage(list);    // remake SourceImage, in case blackout() changed the AttributeList (e.g., removed overlays)
-            } else {
-            }
-        }
-
-    }
 
     private static AttributeList readAttributeList(File currentFile, boolean decompressPixelData) throws IOException, DicomException {
         DicomInputStream i = new DicomInputStream(currentFile);
