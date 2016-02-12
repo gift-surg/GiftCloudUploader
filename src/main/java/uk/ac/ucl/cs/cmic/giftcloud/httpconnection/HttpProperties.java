7f0e25fb616c9fdc90e140b6c56923f9827d5edd
Tom Doel <t.doel@ucl.ac.uk>
2016-02-12 12:23:24 +0000

    Create new properties class for Http-related properties

---
+++ src/main/java/uk/ac/ucl/cs/cmic/giftcloud/httpconnection/HttpProperties.java
@@ -0,0 +1,28 @@
+package uk.ac.ucl.cs.cmic.giftcloud.httpconnection;
+
+/**
+ * Stores configurable properties required for creating an Http connection
+ */
+public class HttpProperties {
+    private final String userAgentString;
+    private final int shortTimeout;
+    private final int longTimeout;
+
+    public HttpProperties(final String userAgentString, final int shortTimeout, final int longTimeout) {
+        this.userAgentString = userAgentString;
+        this.shortTimeout = shortTimeout;
+        this.longTimeout = longTimeout;
+    }
+
+    public String getUserAgentString() {
+        return userAgentString;
+    }
+
+    public int getShortTimeout() {
+        return shortTimeout;
+    }
+
+    public int getLongTimeout() {
+        return longTimeout;
+    }
+}
