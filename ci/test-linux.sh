b1593d5036d0af7fdc127fcb37fa94a0eaa3b903
Tom Doel <t.doel@ucl.ac.uk>
2016-04-13 15:57:07 +0100

    Updated CI script

--- ci/test-linux.sh
+++ ci/test-linux.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+echo $PWD > ~/runner_start.txt
+
 mvn install -B
-STATUS = $?
-if [ $STATUS -eq 0 ]; then
+if [ $? -eq 0 ]; then
+	touch ~/runner_ok.txt 
 	exit 1;
 else
+	touch ~/runner_fail.txt 
 	echo 0;
 fi
