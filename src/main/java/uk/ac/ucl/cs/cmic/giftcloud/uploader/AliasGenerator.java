218a2bf7d23977d5be44ad8ce7f1353385d1c43e
Tom Doel <t.doel@ucl.ac.uk>
2016-04-25 17:18:58 +0100

    Extracted AliasGenerator from AutoUploader

---
+++ src/main/java/uk/ac/ucl/cs/cmic/giftcloud/uploader/AliasGenerator.java
@@ -0,0 +1,83 @@
+package uk.ac.ucl.cs.cmic.giftcloud.uploader;
+
+import uk.ac.ucl.cs.cmic.giftcloud.restserver.*;
+import uk.ac.ucl.cs.cmic.giftcloud.util.*;
+import uk.ac.ucl.cs.cmic.giftcloud.util.Optional;
+
+import java.io.IOException;
+import java.util.*;
+
+/**
+ * Class for assigning pseudonames
+ */
+public class AliasGenerator {
+
+    private final NameGenerator.SubjectNameGenerator subjectNameGenerator;
+    private final SubjectAliasStore subjectAliasStore;
+    private final GiftCloudProperties properties;
+
+    public AliasGenerator(final GiftCloudProperties properties, final GiftCloudReporter reporter) {
+        this.properties = properties;
+        subjectNameGenerator = new NameGenerator.SubjectNameGenerator(properties.getSubjectPrefix());
+        subjectAliasStore = new SubjectAliasStore(new PatientListStore(properties, reporter), reporter);
+    }
+
+    /**
+     * Force saving of the patient list
+     */
+    public void exportPatientList() {
+        subjectAliasStore.exportPatientList();
+    }
+
+    synchronized GiftCloudLabel.SubjectLabel getSubjectName(final GiftCloudServer server, final String projectName, final String patientId, final String patientName) throws IOException {
+        final Optional<GiftCloudLabel.SubjectLabel> existingSubjectLabel = subjectAliasStore.getSubjectAlias(server, projectName, patientId, patientName);
+        if (existingSubjectLabel.isPresent()) {
+            return existingSubjectLabel.get();
+        } else {
+            // Get a list of known subject labels from the server
+            Map<String, String> subjectMapFromServer = server.getListOfSubjects(projectName);
+
+            // Generate a new subject label
+            subjectNameGenerator.updateSubjectNamePrefix(properties.getSubjectPrefix());
+            final GiftCloudLabel.SubjectLabel newSubjectLabel = subjectNameGenerator.getNewName(subjectMapFromServer.keySet());
+
+            // Add the label and its uid alias
+            subjectAliasStore.addSubjectAlias(server, projectName, patientId, newSubjectLabel, patientName);
+            return newSubjectLabel;
+        }
+    }
+
+    GiftCloudLabel.ExperimentLabel getSessionName(final GiftCloudServer server, final String projectName, final GiftCloudLabel.SubjectLabel subjectLabel, final String studyInstanceUid, final XnatModalityParams xnatModalityParams) throws IOException {
+        final Optional<GiftCloudLabel.ExperimentLabel> existingExperimentLabel = subjectAliasStore.getExperimentLabel(server, projectName, subjectLabel, studyInstanceUid);
+        if (existingExperimentLabel.isPresent()) {
+            return existingExperimentLabel.get();
+        } else {
+            // Get a list of known experiment labels from the server for this project
+            final Map<String, String> experimentMapFromServer = server.getListOfSessions(projectName);
+
+            // Generate a new experiment label
+            final GiftCloudLabel.ExperimentLabel newExperimentLabel = subjectNameGenerator.getExperimentNameGenerator(subjectLabel).getNewName(experimentMapFromServer.keySet());
+
+            // Add the label and its uid alias
+            subjectAliasStore.addExperimentAlias(server, projectName, subjectLabel, newExperimentLabel, studyInstanceUid, xnatModalityParams);
+            return newExperimentLabel;
+        }
+    }
+
+    GiftCloudLabel.ScanLabel getScanName(final GiftCloudServer server, final String projectName, final GiftCloudLabel.SubjectLabel subjectLabel, final GiftCloudLabel.ExperimentLabel experimentLabel, final String seriesInstanceUid, final XnatModalityParams xnatModalityParams) throws IOException {
+        final Optional<GiftCloudLabel.ScanLabel> existingScanLabel = subjectAliasStore.getScanLabel(server, projectName, subjectLabel, experimentLabel, seriesInstanceUid);
+        if (existingScanLabel.isPresent()) {
+            return existingScanLabel.get();
+        } else {
+            // Get a list of known scan labels from the server for this project, subject and experiment
+            final Map<String, String> scanMapFromServer = server.getListOfScans(projectName, subjectLabel, experimentLabel);
+
+            // Generate a new scan label
+            final GiftCloudLabel.ScanLabel newScanLabel = subjectNameGenerator.getExperimentNameGenerator(subjectLabel).getScanNameGenerator(experimentLabel).getNewName(scanMapFromServer.keySet());
+
+            // Add the label and its uid alias
+            subjectAliasStore.addScanAlias(server, projectName, subjectLabel, experimentLabel, newScanLabel, seriesInstanceUid, xnatModalityParams);
+            return newScanLabel;
+        }
+    }
+}
