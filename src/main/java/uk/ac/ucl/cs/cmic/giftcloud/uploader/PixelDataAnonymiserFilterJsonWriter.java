60ce40e434adf941ef8edbb13f0c2d275d235b83
Tom Doel <t.doel@ucl.ac.uk>
2016-08-09 18:43:32 +0100

    Fix issue #79: redaction templates now work for integer values

--- src/main/java/uk/ac/ucl/cs/cmic/giftcloud/uploader/PixelDataAnonymiserFilterJsonWriter.java
+++ src/main/java/uk/ac/ucl/cs/cmic/giftcloud/uploader/PixelDataAnonymiserFilterJsonWriter.java
@@ -83,7 +83,7 @@ public class PixelDataAnonymiserFilterJsonWriter {
         return new PixelDataAnonymiseFilter(filterName, requiredTags, redactedShapes);
     }
 
-    private static List<Rectangle2D.Double> parseRedactedShapes(final JSONArray redactedShapesJson) {
+    private static List<Rectangle2D.Double> parseRedactedShapes(final JSONArray redactedShapesJson) throws IOException {
         final List<Rectangle2D.Double> redactedShapes = new ArrayList<Rectangle2D.Double>();
         final Iterator<JSONObject> iterator = redactedShapesJson.iterator();
         while (iterator.hasNext()) {
@@ -93,12 +93,23 @@ public class PixelDataAnonymiserFilterJsonWriter {
         return redactedShapes;
     }
 
-    private static Rectangle2D.Double parseShape(final JSONObject shape) {
-        final double x = ((Double)shape.get(SHAPE_X));
-        final double y = ((Double)shape.get(SHAPE_Y)).doubleValue();
-        final double width = ((Double)shape.get(SHAPE_WIDTH)).doubleValue();
-        final double height = ((Double)shape.get(SHAPE_HEIGHT)).doubleValue();
+    private static Rectangle2D.Double parseShape(final JSONObject shape) throws IOException {
+        final double x = PixelDataAnonymiserFilterJsonWriter.GetDoubleValue(shape, SHAPE_X);
+        final double y = PixelDataAnonymiserFilterJsonWriter.GetDoubleValue(shape, SHAPE_Y);
+        final double width = PixelDataAnonymiserFilterJsonWriter.GetDoubleValue(shape, SHAPE_WIDTH);
+        final double height = PixelDataAnonymiserFilterJsonWriter.GetDoubleValue(shape, SHAPE_HEIGHT);
         return new Rectangle2D.Double(x, y, width, height);
+    }
+
+    private static double GetDoubleValue(final JSONObject shape, final Object key) throws IOException {
+        final Object valueObject = shape.get(key);
+        if (valueObject instanceof Double) {
+            return ((Double)valueObject).doubleValue();
+        } else if (valueObject instanceof Long) {
+            return ((Long)valueObject).doubleValue();
+        } else {
+            throw new IOException("Could not read value from template");
+        }
     }
 
     private static List<PixelDataAnonymiseFilterRequiredTag> parseTagArray(final JSONArray requiredTagsJson) throws IOException {
